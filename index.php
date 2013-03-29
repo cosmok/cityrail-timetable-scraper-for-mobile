372ee3874a3eff1333aa2ecaee56d05cd9a49565
Kesavan Rengarajan <k7@trk7.com>
2013-03-29 15:35:48 +1100

    title more appropriate

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require dirname(__FILE__) . '/stations.php';
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head>
-<title>Mobile Timetable</title>
+<title>Sydney Train Timetable - Mobile Optimized</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <style>
 body {
